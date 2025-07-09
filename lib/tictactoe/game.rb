7b421ba23399215059129021deb059ce7e3c1de0
juhuntenburg <ju.huntenburg@gmail.com>
2025-07-09 10:58:33 +0200

    cccccbvhcclhlghhthhgghnvlliilbltkigdehgdvdgg

--- lib/tictactoe/game.rb
+++ lib/tictactoe/game.rb
@@ -12,14 +12,16 @@ class Game
   def play
     winner = rounds
     puts board
-    puts "Player #{winner.token} won!"
+    winner ? puts("Player #{winner.token} won!") : puts("Nobody won!")
   end
 
   def rounds
+    i = 0
     players.cycle do |player|
       puts board
       field = player.move
       return player if board.won?(field)
+      return false if (i += 1) == 9
     end
   end
 end
